90a747572d6145287a276d9721c1009886fd0815
Yellowtyler <davydovdanya18@gmail.com>
2022-03-31 17:46:12 +0300

    init commit

---
+++ chat-app-message-service/src/test/java/davydov/chat/app/message/service/ChatAppMessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package davydov.chat.app.message.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppMessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
